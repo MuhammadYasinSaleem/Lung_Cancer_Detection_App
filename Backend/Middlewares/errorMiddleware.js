984f309ca05426f63edc606a16c6c06225a5d6eb
Muhammad Yasin <m.yaseensalim@gmail.com>
2025-02-12 12:09:52 +0500

    errorhandler

--- Backend/Middlewares/errorMiddleware.js
+++ Backend/Middlewares/errorMiddleware.js
@@ -25,8 +25,16 @@ export const errorMiddleware = (err, req, res, next) => {
     const message = `Invalid ${err.path}`;
     err = new ErrorHandler(message, 400);
   }
+
+  const errorMessage = err.errors
+    ? Object.values(err.errors)
+        .map((error) => error.message)
+        .join(" ")
+    : err.message;
   return res.status(err.statusCode).json({
-    success:false,
-    message:err.message
-  })
+    success: false,
+    message: errorMessage,
+  });
 };
+
+export default ErrorHandler;
