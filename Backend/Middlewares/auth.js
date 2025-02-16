ac5e090523396adcf4d20a3a931af5fec0335db2
Muhammad Yasin <m.yaseensalim@gmail.com>
2025-02-16 19:32:32 +0500

    authentication

---
+++ Backend/Middlewares/auth.js
@@ -0,0 +1,33 @@
+import {catchAsyncErrors} from './catchAsyncErrors.js'
+import {ErrorHandler} from "../Middlewares/errorMiddleware.js"
+import {jwt} from "jsonwebtoken"
+import {User} from "../models/userSchema.js"
+
+
+export const isAdminauthenticated=catchAsyncErrors(async(req,res,next)=>{
+    const token=req.cookies.adminToken;
+    if(!token){
+        return next(new ErrorHandler("Admin not authenticated!",400))
+    }
+    const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY);
+    req.user=await User.findById(decoded.id)
+    if(req.user.role!=="Admin")
+    {
+        return next(new ErrorHandler(`${req.user.role} not authorized for this resource!`,403))
+    }
+    next();
+})
+
+export const isPatientauthenticated=catchAsyncErrors(async(req,res,next)=>{
+    const token=req.cookies.patientToken;
+    if(!token){
+        return next(new ErrorHandler("User not authenticated!",400))
+    }
+    const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY);
+    req.user=await User.findById(decoded.id)
+    if(req.user.role!=="Patient")
+    {
+        return next(new ErrorHandler(`${req.user.role} not authorized for this resource!`,403))
+    }
+    next();
+})
