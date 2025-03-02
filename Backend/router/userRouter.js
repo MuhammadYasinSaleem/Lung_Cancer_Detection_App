85d9fa2cba0f748c4f98b5538ef911fd1e1c087d
Muhammad Yasin <m.yaseensalim@gmail.com>
2025-03-02 11:41:11 +0500

    logout admin

--- Backend/router/userRouter.js
+++ Backend/router/userRouter.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { AddnewAdmin, getAllDoctors, getUserDetails, login, patientRegister } from "../controller/userController.js";
+import { AddnewAdmin, getAllDoctors, getUserDetails, login, logoutAdmin, patientRegister } from "../controller/userController.js";
 import {isAdminauthenticated,isPatientauthenticated} from "../Middlewares/auth.js"
 
 const UserRouter=express.Router();
@@ -10,4 +10,5 @@ UserRouter.post("/admin/addnew",isAdminauthenticated,AddnewAdmin)
 UserRouter.get("/doctors",getAllDoctors)
 UserRouter.get("/admin/me",isAdminauthenticated,getUserDetails)
 UserRouter.get("/patient/me",isPatientauthenticated,getUserDetails)
+UserRouter.get("/admin/logout",isAdminauthenticated,logoutAdmin)
 export default UserRouter
