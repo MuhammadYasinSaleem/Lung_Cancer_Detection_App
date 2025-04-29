f81151e0be1d5b070d8eb797b85f161e711faaa1
Muhammad Yasin <m.yaseensalim@gmail.com>
2025-04-30 00:54:04 +0500

    Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import Hero from "../components/Hero";
+import Biography from "../components/Biography";
+import Departments from "../components/Departments";
 
-const Home=()=>{
-    return(
-        <></>
-    )
-}
-export default Home
+const Home = () => {
+  return (
+    <>
+      <Hero
+        title={
+          "Welcome to ZeeCare Medical Institute | Your Trusted Healthcare Provider"
+        }
+        imageUrl={"/hero.png"}
+      />
+      <Biography imageUrl={"/about.png"} />
+      <Departments />
+    </>
+  );
+};
+
+export default Home;
