c2877b6f2f81c80e24b13653d36ef2f6ebbaace3
Muhammad Yasin <m.yaseensalim@gmail.com>
2025-04-30 01:16:58 +0500

    Login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,36 @@ import Login from "./pages/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useContext } from "react";
+import { Context } from "./main";
+import { useEffect } from "react";
+import Navbar from "./components/Navbar";
 
 const App = () => {
+  const { isAuthenticated, setIsAuthenticated, setUser } =
+    useContext(Context);
+    useEffect(() => {
+      const fetchUser = async () => {
+        try {
+          const response = await axios.get(
+            "http://localhost:5000/api/v1/user/patient/me",
+            {
+              withCredentials: true,
+            }
+          );
+          setIsAuthenticated(true);
+          setUser(response.data.user);
+        } catch (error) {
+          setIsAuthenticated(false);
+          setUser({});
+        }
+      };
+      fetchUser();
+    }, [isAuthenticated]);
   return (
     <>
       <Router>
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
