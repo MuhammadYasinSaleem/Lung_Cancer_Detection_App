4a00bfaa2044f929471736a7cf191f6f9761c0c3
Muhammad Yasin <m.yaseensalim@gmail.com>
2025-05-04 14:28:39 +0500

    dashboard

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -142,3 +142,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
--- lung_cancer_api/app/main.py
+++ lung_cancer_api/app/main.py
@@ -23,3 +23,44 @@ async def predict(file: UploadFile = File(...)):
     result = "Cancer Detected" if prediction[0][0] > 0.5 else "No Cancer Detected"
     
     return JSONResponse(content={"result": result, "confidence": float(prediction[0][0])})
+
+
+
+# @app.post("/predict")
+# async def predict(file: UploadFile = File(...)):
+#     image = Image.open(file.file).convert("RGB").resize((512,512))
+#     img_array = np.array(image) / 255.0
+#     img_tensor = tf.convert_to_tensor(img_array, dtype=tf.float32)
+#     img_tensor = tf.image.random_contrast(img_tensor, lower=0.8, upper=1.2)
+#     img_tensor = tf.expand_dims(img_tensor, axis=0)
+    
+#     prediction = model.predict(img_tensor)
+#     result = "Cancer Detected" if prediction[0][0] > 0.5 else "No Cancer Detected"
+#     return JSONResponse(content={"result": result, "confidence": float(prediction[0][0])})
+
+
+
+
+
+
+
+
+
+# class_names = ["No Cancer Detected", "Cancer Detected"]
+
+# @app.post("/predict")
+# async def predict(file: UploadFile = File(...)):
+#     image = Image.open(file.file).convert("RGB").resize((512, 512))
+#     img_array = np.array(image) / 255.0
+#     img_tensor = tf.convert_to_tensor(img_array, dtype=tf.float32)
+#     img_tensor = tf.image.random_contrast(img_tensor, lower=0.8, upper=1.2)
+#     img_tensor = tf.expand_dims(img_tensor, axis=0)
+
+#     predictions = model.predict(img_tensor)
+#     index = int(np.argmax(predictions))
+#     confidence = float(predictions[0][index])
+#     result = class_names[index]
+
+#     return JSONResponse(content={"result": result, "confidence": confidence})
+
+
